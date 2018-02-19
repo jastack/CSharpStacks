2d108cddf56181d4daf3e47b5aba060e1d239910
James Stack <JStack@mathematica.net>
2018-02-19 13:49:32 -0800

    first commit

---
+++ Stacks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newStack = new Stack();
+            newStack.Push(1);
+            newStack.Push(2);
+            newStack.Push(3);
+            Console.WriteLine(newStack.Pop());
+            Console.WriteLine(newStack.Pop());
+            newStack.Clear();
+            newStack.Push("hi");
+            newStack.Push(null);
+            Console.WriteLine(newStack.Pop());
+            Console.WriteLine(newStack.Pop());
+        }
+    }
+}
---
+++ Stacks/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks
+{
+    public class Stack
+    {
+        private List<object> MyStack { get; set; }
+
+        public Stack()
+        {
+            MyStack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                Console.WriteLine("Can't add null to stack");
+            else
+                MyStack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            var index = MyStack.Count - 1;
+            try
+            {
+                var LastElement = MyStack[index];
+                MyStack.RemoveAt(index);
+                return LastElement;
+            }
+            catch (Exception)
+            {
+                var message = "Stack is empty!";
+                Console.WriteLine(message);
+                return message;
+            }
+                
+        }
+
+        public void Clear()
+        {
+            MyStack = new List<object>();
+        }
+    }
+}
